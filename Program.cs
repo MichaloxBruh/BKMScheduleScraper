58fa816815801577f574a191df39ed88a38a6455
MichaloxBruh <79116858+MichaloxBruh@users.noreply.github.com>
2025-01-16 09:37:03 +0100

    Basic functions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string response_01;
+string response_02;
+string line_BKM;
+
 Console.WriteLine("Scrape a BKM or WE schedule?");
 response_01 = Console.ReadLine();
-if (response_01 = "BKM") ;
+
+//i'm sooooo stupid...
+if (response_01 == "BKM");
+{
+    Console.WriteLine("Line Number");
+    line_BKM = Console.ReadLine();
+}
+if (response_01 == "WE") ;
+{
+    Console.WriteLine("Wschód Express doesn't work currently, sorry!");
+}
