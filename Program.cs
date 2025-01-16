d082f9916771bbb810a53d7ec54741acfb9793d3
MichaloxBruh <79116858+MichaloxBruh@users.noreply.github.com>
2025-01-16 08:42:56 +0100

    fixing my dumbass mistakes

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string response_01;
+Console.WriteLine("Scrape a BKM or WE schedule?");
+response_01 = Console.ReadLine();
+if (response_01 = "BKM") ;
